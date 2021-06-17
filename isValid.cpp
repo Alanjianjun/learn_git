26b6e3d604bedb7d3aae0fde9436dfb764238464
alan <enggg3112@qq.com>
2021-06-17 23:48:29 +0800

    final exam practise

--- isValid.cpp
+++ isValid.cpp
@@ -1,4 +1,4 @@
- bool isValid(string s) {
+bool isValid(string s) {
         stack<char> help;
         unordered_map<char, char> tmp{{')','('}, {'}','{'}, {']','['}};
         for(int i = 0; i < s.size(); i++)
@@ -12,3 +12,24 @@
         if(help.empty())  return true;
         return false;
     }
+
+class Solution{
+public:
+    bool isValid(string s) {
+        stack<char> help;
+        unordered_map<char, char> tmp{{')','('}, {'}','{'}, {']','['}};
+        for(int i = 0; i < s.size(); i++)
+        {
+            if(help.empty()){
+                if (s[i]==')' || s[i]=='}' ||s[i]==']') {return false;}
+                else{help.push(s[i]);}
+            }  
+            else{
+                if(tmp[s[i]] == help.top()) help.pop();
+                else help.push(s[i]);
+            }
+        }
+        if(help.empty())  return true;
+        return false;
+    }
+};
--- twoSum.cpp
+++ twoSum.cpp
@@ -17,3 +17,19 @@ public:
         return ret;
     }
 };
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        unordered_map<int,int> hash_table;
+        int myTarget=0;
+        for(int i=0;i<nums.size();i++){
+            myTarget=target-nums[i];
+            if (hash_table.count(myTarget)){
+                return {hash_table[myTarget],i};
+            }
+            hash_table[nums[i]]=i;
+        }
+        return {};
+    }
+};
